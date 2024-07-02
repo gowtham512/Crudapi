68deade073fda015559a86aad0377d42354bd999
Pateel Sai Gowtham Reddy <113625678+gowtham512@users.noreply.github.com>
2024-07-02 07:53:13 +0530

    Add files via upload

---
+++ Crud api/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
